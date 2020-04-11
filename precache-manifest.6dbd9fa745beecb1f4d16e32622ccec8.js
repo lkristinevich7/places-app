ba36b589bfc52c5d43c84da9137fd135581e561b
Liudmila Krystsinevich <lkristinevich.webdev@gmail.com>
2020-04-11 17:40:20 +0200

    Updates

---
+++ precache-manifest.6dbd9fa745beecb1f4d16e32622ccec8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa20f8622bcfa4bd3dda4c37bb57595",
+    "url": "/places-app/index.html"
+  },
+  {
+    "revision": "41e98100941dbbdf29df",
+    "url": "/places-app/static/css/main.fd04ebc7.chunk.css"
+  },
+  {
+    "revision": "5c6b995b2da2fd5c3956",
+    "url": "/places-app/static/js/2.243fb9e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/places-app/static/js/2.243fb9e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41e98100941dbbdf29df",
+    "url": "/places-app/static/js/main.1b26f97c.chunk.js"
+  },
+  {
+    "revision": "ba307771316435cfc202",
+    "url": "/places-app/static/js/runtime-main.3202b7e9.js"
+  }
+]);
